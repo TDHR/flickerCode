785e1aaeb93df42abed1c5c298f57aacddbdd004
TDHR <978481876@qq.com>
2018-12-25 15:39:28 +0800

    页面修改

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -271,21 +271,20 @@ exports.drawingAsset = async function (ctx) {
 };
 //发送提取请求
 const sendDrawingRequest = async function (address,asset,number,openid,nickname) {
-    let  data = {
-        address:address,
-        asset:asset,
-        number:number,
-        openid:openid,
-        nickname:nickname
-    }
-    data = JSON.stringify(data);
+
     return new Promise((resolve,reject)=> {
         request
             .post('127.0.0.1:3009/transfer/drawing')
             .set('Accept', 'application/json')
             .set('Content-type','application/json')
-            .set('Content-Length',Buffer.byteLength(data))
-            .send(data)
+
+            .send({
+                address:address,
+                asset:asset,
+                number:number,
+                openid:openid,
+                nickname:nickname
+            })
             .end(function (error, result) {
                 if(error){
                     saveDrawingErrorMessage(openid, nickname,asset, number,error.status,address);
