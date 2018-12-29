d6335fe84fa168039b85dcb9e080bf251d4d82a1
TDHR <978481876@qq.com>
2018-12-29 15:40:27 +0800

    判断账号状态

--- server/router/login.js
+++ server/router/login.js
@@ -5,10 +5,13 @@ router.get('/login',login.index);
 router.post('/login',login.login);
 router.post('/getPhoneCode',login.getPhoneCode);//获取手机验证码
 router.get('/drawingTest',login.drawingTest);
-router.get('/drawing',login.drawing);//提取页面
 
-router.post('/drawing',login.drawingTest);//提取方法
 
-// router.post('/drawing',login.drawingAsset);//提取方法
+router.get('/drawing',login.drawingTest);//提取页面
+// router.get('/drawing',login.drawing);//提取页面
+
+
+
+router.post('/drawing',login.drawingAsset);//提取方法
 
 module.exports = router;
